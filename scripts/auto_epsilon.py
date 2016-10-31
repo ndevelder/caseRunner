d878315d7ce4b9c82706675c2020b03470c6f8d4
Nathaniel deVelder [develder@ecs.umass.edu] <nd29a@ghpcc06.umassrc.org>
2016-10-31 14:05:25 -0400

    Main script updates

--- scripts/auto_epsilon.py
+++ scripts/auto_epsilon.py
@@ -2,11 +2,11 @@
 # Find epsilon boundary conditions
 
 from subprocess import call
-import os
+import os,sys
 import numpy as np
 
 # Function to replace epsilon expression
-def replace_eps(case,cl,ce,l,rp,cc):
+def replace_eps(case,cl,ce,l,rp,cc,epsName):
 	epsFile = case+'/0/epsilon'
 
 	f = open(epsFile,'r')
@@ -16,11 +16,16 @@ def replace_eps(case,cl,ce,l,rp,cc):
  	if (rp < 25.0):
 		Sr = pow(50.0/float(rp),2)
 	else:
-		Sr = pow(100.0/float(rp),2)
+		Sr = 100.0/float(rp)
 
-	hellstenEpsExpression = str(cc)+'*'+str(Sr)+'*'+'pow((nu+0.833*nut)*mag(vorticity),2)/nu'
-	newEpsExpression = str(ce)+'*pow(internalField(k),1.5)/(0.41*'+str(ce)+'*'+str(l)+')'
-	newdata = filedata.replace("<valueExpressionHere>",hellstenEpsExpression)
+	if epsName == 'hellsten':
+		EpsExpression = str(cc)+'*'+str(Sr)+'*'+'pow((nu+0.833*nut)*mag(vorticity),2)/nu'
+	elif epsName == 'eca':
+		EpsExpression = str(ce)+'*pow(internalField(k),1.5)/(0.41*'+str(ce)+'*'+str(l)+')'
+	else:
+		EpsExpression = str(cc)+'*'+str(Sr)+'*'+'pow((nu+0.833*nut)*mag(vorticity),2)/nu'   
+	
+	newdata = filedata.replace("<valueExpressionHere>",EpsExpression)
 
 	f = open(epsFile,'w')
 	f.write(newdata)
@@ -40,9 +45,13 @@ def replace_tpphi(case,value):
         f.write(newdata)
         f.close()
 
-
 # Source the foam-extend bashrc file
 call(['/bin/bash','-i','-c','fe32'])
+
+# Input arguments
+epsType=sys.argv[1]
+submitJob=sys.argv[2]
+decomposeJob=sys.argv[3]
 
 # Set up case dirs
 runDir = os.environ.get('FOAM_RUN')
@@ -56,10 +65,10 @@ constantE   = np.array([1,1,1,1])
 constantL   = np.array([1,1,1,1])
 lengthScale = np.array([0.1,0.1,0.1,0.1])
 phiValue    = np.array([0.09743541, 0.22499132, 0.31663022, 0.40199916])
-rPlus 	    = np.array([20.0,45.0,70.0,100.0])
-constantC   = np.array([0.01,0.1,1.0,3.0,5.0])
+rPlus 	    = np.array([999.9])
+constantC   = np.array([0.999])
 
-nCases = phiValue.shape[0]
+nCases = rPlus.shape[0]
 nCons  = constantC.shape[0]
 
 # Copy and run loop
@@ -70,5 +79,12 @@ for c in range(1,nCons+1):
 		result = call('cp -R '+caseTemplatePath+'/ '+newCase,shell=True)
 
 		if result == 0:
-			replace_eps(newCase,constantL[n-1],constantE[n-1],lengthScale[n-1],rPlus[n-1],constantC[c-1])
+			replace_eps(newCase,constantL[n-1],constantE[n-1],lengthScale[n-1],rPlus[n-1],constantC[c-1],epsType)
 			replace_tpphi(newCase,phiValue[n-1])
+
+			if decomposeJob == 'decompose':
+				decompresult = call('decomposePar -case '+newCase,shell=True)
+			
+			if decompresult == 0:
+				if submitJob == 'submit':
+					bsubresult = call('cd '+newCase+' && bsub < job.mpi && cd ..',shell=True)
