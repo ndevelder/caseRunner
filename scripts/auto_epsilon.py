3b5f1efb4006cb5646378bfbd819ff72c93cf530
Nate deVelder <ndevelde@umass.edu>
2016-10-31 14:26:39 -0400

    Fixed function name

--- scripts/auto_epsilon.py
+++ scripts/auto_epsilon.py
@@ -46,7 +46,7 @@ def replace_tpphi(case,value):
         f.close()
 
 # Function to replace k expression
-def replace_eps(case,rp):
+def replace_k(case,rp):
         kFile = case+'/0/k'
 
         f = open(kFile,'r')
@@ -81,8 +81,8 @@ constantE   = np.array([1,1,1,1])
 constantL   = np.array([1,1,1,1])
 lengthScale = np.array([0.1,0.1,0.1,0.1])
 phiValue    = np.array([0.09743541, 0.22499132, 0.31663022, 0.40199916])
-rPlus 	    = np.array([20.0,45.0,70.0,100.0])
-constantC   = np.array([0.01,0.1,1.0,2.0,3.0])
+rPlus 	    = np.array([999.99])
+constantC   = np.array([0.9999])
 
 nCases = rPlus.shape[0]
 nCons  = constantC.shape[0]
